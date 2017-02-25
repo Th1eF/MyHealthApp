fca6f9eb45c39977ec22a04b9fffee7a65cdeb7f
Th1eF <phillip_osial@hotmail.com>
2017-02-25 15:14:31 -0500

    Added hashing and salt to user password Updated gitignore

--- www/php/createNewUser.php
+++ www/php/createNewUser.php
@@ -12,15 +12,25 @@ if (!$link) {
 }else{
     echo "Successful connection";
 }
+// Security stuff
 $auth = bin2hex(openssl_random_pseudo_bytes(16));
+$password = $_POST["password"];
+$passwordSalt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+
+$options = [
+    'cost' => 12,
+    'salt' => $passwordSalt,
+];
+
+$password = password_hash($password, PASSWORD_BCRYPT, $options);
+
 $firstName = $_POST["firstName"];
 $lastName = $_POST["lastName"];
 $emailAddress = $_POST["emailAddress"];
-$password = $_POST["password"];
 
-$statement = "INSERT INTO user (auth, firstName, lastName, email, password) VALUES (?, ?, ?, ?, ?)";
+$statement = "INSERT INTO user (auth, firstName, lastName, email, password, passwordSalt) VALUES (?, ?, ?, ?, ?, ?)";
 if($stmt = $link->prepare($statement)){
-    $stmt->bind_param("sssss", $auth, $firstName, $lastName, $emailAddress, $password);
+    $stmt->bind_param("ssssss", $auth, $firstName, $lastName, $emailAddress, $password, $passwordSalt);
     if(!$stmt->execute()) throw new Exception($stmt->error());
     $stmt->free_result();
     $stmt->close();
