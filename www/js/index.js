30433ca284a8396621d98a6c5e38ce833899f3b4
Th1eF <phillip_osial@hotmail.com>
2017-02-27 23:48:33 -0500

    Uploading of personal data completed, each user has their own table Created a config file to share variables Some refactoring

--- www/js/index.js
+++ www/js/index.js
@@ -18,22 +18,24 @@ var app = {
         app.signUpController = new SignIn.SignUpController();
         app.signInController = new SignIn.SignInController();
 
+        sensorData.init();
+
         //Check if authToken in localstorage
         if(typeof(Storage !== "undefined")){
-            var authToken = localStorage.getItem("authToken");
-            var emailAddress = localStorage.getItem("email");
-            //console.log(JSON.stringify(authToken));
-            if(authToken && emailAddress){
+            if(Config.authToken && Config.emailAddress){
                 console.log("An auth token and email exists");
                 $.ajax({
                     type: 'GET',
                     url: 'http://138.197.130.124/verifyKeepLogin.php',
                     data: {
-                        authToken: authToken,
-                        emailAddress: emailAddress
+                        authToken: Config.authToken,
+                        emailAddress: Config.emailAddress
                     },
                     dataType: 'json',
                     success: function(user){
+                        Config.loggedIn = true;
+                        Config.authToken = localStorage.getItem("authToken");
+                        Config.emailAddress = localStorage.getItem("email");
                         console.log("Logged in user successfully");
                         console.log("navigating to main page...");
                         console.log(user);
@@ -56,12 +58,13 @@ var app = {
                     type: 'POST',
                     url: 'http://138.197.130.124/logout.php',
                     data: {
-                        emailAddress: emailAddress
+                        emailAddress: Config.emailAddress
                     },
                     success: function(){
                         console.log("Logged out user successfully");
                         $('.panel_userName').text("");
                         localStorage.clear();
+                        Config.loggedIn = false;
                     },
                     error: function(xhr, ajaxOptions, thrownError){
                         console.log("Error Code: " + xhr.status);
@@ -315,115 +318,6 @@ var app = {
             console.log("keep signed in clicked");
         });
 
-        //-----------------------------------------------------------------------
-        //Pedometer
-        var lastTimeStamp = new Date().getTime();
-        var lastLat;
-        var lastLong;
-        var lastSpeed;
-        var lastStep;
-
-        var totalSteps = 0;
-        if(window.localStorage != undefined){
-            var localSteps = parseInt(window.localStorage.getItem("Steps"));
-            if(!isNaN(localSteps)){
-                totalSteps = localSteps;
-            }
-
-            $('#steps').text("Steps: " + totalSteps);
-        }
-
-        var pedometerSuccess = function (pedometerData) {
-            /*console.log(
-                'Start Date: ' + pedometerData.startDate + '\n' +
-                'End Date: ' + pedometerData.endDate + '\n' +
-                '# of Steps: ' + pedometerData.numberOfSteps + '\n'
-                /*'Distance: ' + pedometerData.distance + '\n' +
-                'Floors Ascended: ' + pedometerData.floorsAscended + '\n' +
-                'Floors Descended: ' + pedometerData.floorsDescended + '\n')*/
-
-            if(window.localStorage != undefined){
-                var updatedSteps = totalSteps+pedometerData.numberOfSteps;
-                window.localStorage.setItem("Steps", updatedSteps);
-                lastStep = updatedSteps;
-                $('#steps').text("Steps: " + updatedSteps);
-            }
-        };
-
-        pedometer.isStepCountingAvailable(function(){
-            console.log( "Pedometer step counting is available" );
-        }, function(){
-            console.log( "Pedometer step counting is NOT available" );
-        });
-
-        var pedometerError = function(error){
-            console.log('error: ' + error)
-        };
-        pedometer.startPedometerUpdates(pedometerSuccess, pedometerError);
-        //Pedometer
-        //-----------------------------------------------------------------------
-
-        //-----------------------------------------------------------------------
-        //Geolocation
-        var geoSuccess = function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            var speed = position.coords.speed;
-            lastLat = latitude;
-            lastLong = longitude;
-            lastSpeed = speed;
-            lastTimeStamp = position.timestamp;
-            $('#lat').text("Latitude: " + latitude);
-            $('#long').text("Longitude: " + longitude);
-            $('#speed').text("Speed: " + speed + " m/s");
-            /*console.log('Latitude: '          + position.coords.latitude          + '\n' +
-                'Longitude: '         + position.coords.longitude         + '\n' +
-                'Altitude: '          + position.coords.altitude          + '\n' +
-                'Accuracy: '          + position.coords.accuracy          + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                'Heading: '           + position.coords.heading           + '\n' +
-                'Speed: '             + position.coords.speed             + '\n' +
-                'Timestamp: '         + position.timestamp                + '\n');*/
-        };
-
-        function geoError(error) {
-            console.log('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-        }
-
-        var geoOptions = {
-            maximumAge: 3600000,
-            timeout: 1000,
-            enableHighAccuracy: true
-        };
-
-        navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
-        //Geolocation
-        //-----------------------------------------------------------------------
-
-        //-----------------------------------------------------------------------
-        //Heart Beat
-        $('#measureBPM').on('click', function(){
-            function heartSuccess(bpm){
-                $('#heartRate').text('Heart Rate: ' + bpm);
-
-                /*uploadData(lastTimeStamp, lastLat, lastLong, lastSpeed, lastStep, bpm, "", "", function(callback){
-                   console.log(callback);
-                });*/
-            }
-
-            function heartError(){
-                alert("Not possible to measure your heart rate");
-            }
-
-            var heartOptions = {
-                seconds: 10,
-                fps: 30
-            };
-
-            heartbeat.take(heartOptions, heartSuccess, heartError);
-        });
-
         //Send data to cloud database
         /*function uploadData(timestamp, latitude, longitude, speed, steps, bpm, visit, duration, callback){
             $.ajax({
@@ -558,26 +452,6 @@ var app = {
                 return false;
             }
         }
-        /*
-        window.setInterval(function(){
-            var mcTime = checkMcDonalds();
-            var lakeheadTime = checkLakehead();
-            if(mcTime){
-                uploadData(lastTimeStamp, lastLat, lastLong, lastSpeed, lastStep, "", "McDonalds", mcTime, function(callback){
-                 console.log(callback);
-                 });
-                console.log(mcTime)
-            }
-            if(lakeheadTime){
-                uploadData(lastTimeStamp, lastLat, lastLong, lastSpeed, lastStep, "", "Lakehead Hangar", lakeheadTime, function(callback){
-                    console.log(callback);
-                });
-            }
-
-            uploadData(lastTimeStamp, lastLat, lastLong, lastSpeed, lastStep, "", "", "", function(callback){
-                console.log(callback);
-            });
-        }, 1000)*/
     },
 
 
