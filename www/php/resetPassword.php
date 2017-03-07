b5f613701d5ed7095d7ac2207d6714fb85f916ce
Th1eF <phillip_osial@hotmail.com>
2017-03-07 18:33:03 -0500

    Fixed resetPassword not checking if email exists first Began next part which verifies the code sent to email

--- www/php/resetPassword.php
+++ www/php/resetPassword.php
@@ -24,15 +24,17 @@ $options = [
 //TODO Finish assigning a temporary password (1 time use) in database for the user once email verified
 $newPassword = password_hash($tempPassword, PASSWORD_BCRYPT, $options);
 
-$statement = "SELECT firstName, lastName FROM user WHERE email = ?";
+$statement = "SELECT email, firstName, lastName FROM user WHERE email = ?";
 if($stmt = $link->prepare($statement)){
     $stmt->bind_param("s", $emailAddress);
     if(!$stmt->execute()) throw new Exception($stmt->error());
     $result = $stmt->get_result();
     while($row = $result->fetch_assoc()){
+        $emailExists = $row['email'];
         $firstName = $row['firstName'];
         $lastName = $row['lastName'];
     }
+    if(!$emailExists) throw new Exception($stmt->error());
     //E-Mail System
     //********************************************************
     require_once 'swiftmailer/lib/swift_required.php';
@@ -45,13 +47,19 @@ if($stmt = $link->prepare($statement)){
 
     $message = Swift_Message::newInstance("MyHealthApp Password Reset Request")
         ->setFrom(array('web.health.noreply@gmail.com' => "MyHealthApp Account Support"))
-        ->setTo(array($emailAddress))
+        ->setTo(array($emailExists))
         ->setBody("Hello $firstName $lastName!\n\nWe have noticed that you requested to reset your password.\nIf this request wasn't made by you, please ignore this email and report the issue.\n\nUse this temporary password to login: $tempPassword\n\nPlease change your password immediately after logging in by following the instructions.");
 
     if(!$result = $mailer->send($message)) throw new Exception($stmt->error());
 
     //E-Mail System END
     //********************************************************
+    $statement = "UPDATE user SET passwordResetToken = ?, passwordResetTokenDate = ? WHERE email = ?";
+    if($stmt = $link->prepare($statement)){
+        $stmt->bind_param("sss", $tempPassword, time(), $emailExists);
+        if(!$stmt->execute()) throw new Exception($stmt->error());
+        $stmt->free_result();
+    }
     $stmt->free_result();
     $stmt->close();
 }
