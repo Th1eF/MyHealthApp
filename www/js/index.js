364e7398ff278533577d3f27f374b8ba9e6a8926
Th1eF <phillip_osial@hotmail.com>
2017-03-06 19:46:21 -0500

    Refactoring including: moved where graphs are generated added ability to change your password once you are signed into the app, need your old password to successfully set a new one added uploadData toggle switch so we don't constantly upload data when testing the application

--- www/js/index.js
+++ www/js/index.js
@@ -75,204 +75,6 @@ var app = {
             });
         }
 
-
-
-        $('#toMain').on('click', function(){
-            $("#statsContainer").hide();
-            clearGraphs();
-        });
-
-        function clearGraphs(){
-            $('#visitStats').empty();
-            $('#healthStats').empty();
-        }
-
-        $('#statsPage').on('pageshow', function(e){
-           console.log('page shown');
-            $("#statsContainer").show();
-
-            $("#radio-choice-v-6a").click(function(){
-               console.log("one week checked");
-                $("#timePopContent").popup('close');
-                var weekTime = 604800000;
-                var currTime = lastTimeStamp;
-                var pastTime = currTime - weekTime;
-                //datePast, dateCurr
-                getData(pastTime, currTime, function(callback){
-                    console.log(callback);
-                    var avgBPM = callback["Heart Rate"].BPMAvg;
-                    var gymVisit = callback["Lakehead Hangar"].totalVisits;
-                    var gymDur = callback["Lakehead Hangar"].avgDuration;
-                    var mcVisit = callback["McDonalds"].totalVisits;
-                    var mcDur = callback["McDonalds"].avgDuration;
-                    console.log(avgBPM + " " + gymVisit + " " +  gymDur + " " +  mcVisit + " " +  mcDur);
-                    if((mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null) && (gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null)) {
-                        var visitStats = [['No Statistics Found', 0]];
-                        var healthStats = [['Steps', lastStep], ['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null){
-                        var visitStats = [['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null){
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else{
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur], ['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }
-                });
-            });
-
-            $("#radio-choice-v-6b").click(function(){
-                console.log("3 days");
-                $("#timePopContent").popup('close');
-                var threeDayTime = 259200000;
-                var currTime = lastTimeStamp;
-                var pastTime = currTime - threeDayTime;
-                //datePast, dateCurr
-                //var visitStats = [['Gym', 4],['McDonalds', 2]];
-                //var healthStats = [['Steps', 500],['Avg BPM', 70]];
-                getData(pastTime, currTime, function(callback){
-                    console.log(callback);
-                    var avgBPM = callback["Heart Rate"].BPMAvg;
-                    var gymVisit = callback["Lakehead Hangar"].totalVisits;
-                    var gymDur = callback["Lakehead Hangar"].avgDuration;
-                    var mcVisit = callback["McDonalds"].totalVisits;
-                    var mcDur = callback["McDonalds"].avgDuration;
-                    console.log(avgBPM + " " + gymVisit + " " +  gymDur + " " +  mcVisit + " " +  mcDur);
-                    if((mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null) && (gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null)) {
-                        var visitStats = [['No Statistics Found', 0]];
-                        var healthStats = [['Steps', lastStep], ['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null){
-                        var visitStats = [['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null){
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else{
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur], ['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }
-                });
-            });
-
-            $("#radio-choice-v-6c").click(function(){
-                console.log("1 day");
-                $("#timePopContent").popup('close');
-                var dayTime = 86400000;
-                var currTime = lastTimeStamp;
-                var pastTime = currTime - dayTime;
-                //datePast, dateCurr
-                getData(pastTime, currTime, function(callback){
-                    console.log(callback);
-                    var avgBPM = callback["Heart Rate"].BPMAvg;
-                    var gymVisit = callback["Lakehead Hangar"].totalVisits;
-                    var gymDur = callback["Lakehead Hangar"].avgDuration;
-                    var mcVisit = callback["McDonalds"].totalVisits;
-                    var mcDur = callback["McDonalds"].avgDuration;
-                    console.log(avgBPM + " " + gymVisit + " " +  gymDur + " " +  mcVisit + " " +  mcDur);
-                    if((mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null) && (gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null)) {
-                        var visitStats = [['No Statistics Found', 0]];
-                        var healthStats = [['Steps', lastStep], ['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null){
-                        var visitStats = [['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null){
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else{
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur], ['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }
-                });
-            });
-
-            $("#radio-choice-v-6d").click(function(){
-                console.log("one hour");
-                $("#timePopContent").popup('close');
-                var hourTime = 3600000;
-                var currTime = lastTimeStamp;
-                var pastTime = currTime - hourTime;
-                //datePast, dateCurr
-                getData(pastTime, currTime, function(callback){
-                    console.log(callback);
-                    var avgBPM = callback["Heart Rate"].BPMAvg;
-                    var gymVisit = callback["Lakehead Hangar"].totalVisits;
-                    var gymDur = callback["Lakehead Hangar"].avgDuration;
-                    var mcVisit = callback["McDonalds"].totalVisits;
-                    var mcDur = callback["McDonalds"].avgDuration;
-                    console.log(avgBPM + " " + gymVisit + " " +  gymDur + " " +  mcVisit + " " +  mcDur);
-                    if((mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null) && (gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null)) {
-                        var visitStats = [['No Statistics Found', 0]];
-                        var healthStats = [['Steps', lastStep], ['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(gymVisit == 0 || gymVisit == null || gymDur == 0 || gymDur == null){
-                        var visitStats = [['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else if(mcVisit == 0 || mcVisit == null || mcDur == 0 || mcDur == null){
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }else{
-                        var visitStats = [['Lakehead Hangar', gymVisit], ['Gym Avg(s)', gymDur], ['McDonalds', mcVisit], ['McD Avg(s)', mcDur]];
-                        var healthStats = [['Steps', lastStep],['Avg BPM', avgBPM]];
-                        createGraphs(visitStats, healthStats);
-                    }
-                });
-            });
-
-            function createGraphs(visitStats, healthStats){
-                clearGraphs();
-
-                $('#visitStats').jqplot([visitStats], {
-                    animate: !$.jqplot.use_excanvas,
-                    title:'Visit Stats',
-                    seriesDefaults:{
-                        renderer:$.jqplot.BarRenderer,
-                        pointLabels: {show: true},
-                        rendererOptions:{
-                            varyBarColor: true
-                        }
-                    },
-                    axes:{
-                        xaxis:{
-                            renderer: $.jqplot.CategoryAxisRenderer
-                        }
-                    }
-                });
-
-                $('#healthStats').jqplot([healthStats], {
-                    animate: !$.jqplot.use_excanvas,
-                    title:'Health Stats',
-                    seriesDefaults:{
-                        renderer:$.jqplot.BarRenderer,
-                        pointLabels: {show: true},
-                        rendererOptions:{
-                            varyBarColor: true
-                        }
-                    },
-                    axes:{
-                        xaxis:{
-                            renderer: $.jqplot.CategoryAxisRenderer
-                        }
-                    }
-                });
-            }
-        });
-
-
         $(document).on("pagecontainerbeforeshow", function (event, ui) {
             if (typeof ui.toPage == "object") {
                 switch (ui.toPage.attr("id")) {
@@ -287,16 +89,43 @@ var app = {
             }
         });
 
+        //TODO fix this up possibly with a controller and add a splash/toast confirmation
+        $('#submitNewPass').on('click', function(){
+            var oldPassword = $('#old-password').val().trim();
+            var newPassword = $('#new-password').val().trim();
+            var newPasswordConfirm = $('#new-password-confirm').val().trim();
+            if(newPassword === newPasswordConfirm){
+                console.log(oldPassword);
+                console.log(newPassword);
+                console.log(newPasswordConfirm);
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://138.197.130.124/setNewPassword.php',
+                    data: {
+                        oldPassword: oldPassword,
+                        newPassword: newPassword,
+                        auth: Config.authToken
+                    },
+                    success: function(){
+                        console.log("New password set successfully")
+                        $('#old-password').val("");
+                        $('#new-password').val("");
+                        $('#new-password-confirm').val("")
+                    },
+                    error: function(xhr, ajaxOptions, thrownError){
+                        console.log("Error Code: " + xhr.status);
+                        console.log("Error Response: " + xhr.responseText);
+                        console.log("Thrown Error: " + thrownError);
+                    }
+                });
+            }
+        });
+
         $('#sign-in').on('pageshow', function (e) {
             app.signInController.init();
             app.signInController.$btnSubmit.off("tap").on("tap", function () {
                 app.signInController.onSignIn();
             });
-        });
-
-        $('#statsPage').on('pagehide', function(){
-            $("#statsContainer").hide();
-            clearGraphs();
         });
 
         $('#sign-up').on('pageshow', function (e) {
@@ -326,52 +155,6 @@ var app = {
         $('#dlg-pwd-reset-sent').click(function(){
             $.mobile.navigate("#sign-in", {transition: "slideup"});
         });
-
-        //Send data to cloud database
-        /*function uploadData(timestamp, latitude, longitude, speed, steps, bpm, visit, duration, callback){
-            $.ajax({
-                type: 'POST',
-                url: 'http://138.197.130.124/uploadData.php',
-                data: {
-                    timestamp: timestamp,
-                    latitude: latitude,
-                    longitude: longitude,
-                    speed: speed,
-                    steps: steps,
-                    bpm: bpm,
-                    visit: visit,
-                    duration: duration
-                },
-                success: callback,
-                error: function(xhr, ajaxOptions, thrownError){
-                    console.log("Error Code: " + xhr.status);
-                    console.log("Error Response: " + xhr.responseText);
-                    console.log("Thrown Error: " + thrownError);
-                    callback(false);
-                }
-            });
-        }*/
-
-        //Get data from cloud database
-        function getData(datePast, dateCurr, callback) {
-            $.ajax({
-                type: 'GET',
-                url: 'http://138.197.130.124/getData.php',
-                data: {
-                    datePast: datePast,
-                    dateCurr: dateCurr
-                },
-                dataType: 'json',
-                success: callback,
-                error: function(xhr, ajaxOptions, thrownError){
-                    console.log("Error Code: " + xhr.status);
-                    console.log("Error Response: " + xhr.responseText);
-                    console.log("Thrown Error: " + thrownError);
-                    callback(false);
-                }
-            });
-        }
-
 
         //Haversine formula
         function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
