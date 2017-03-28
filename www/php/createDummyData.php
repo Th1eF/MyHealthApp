0cccddb0c7a9a78b5965b1a77d0592fc87c87e2c
Th1eF <phillip_osial@hotmail.com>
2017-03-28 01:16:00 -0400

    Merge branch 'kalle-dev' of https://github.com/Th1eF/MyHealthApp into PhilDev
    # Conflicts:
    #	platforms/android/assets/www/css/index.css

--- www/php/createDummyData.php
+++ www/php/createDummyData.php
@@ -17,7 +17,7 @@ $oneHour = 3600000;
 $tenMin = 600000;
 
 $possibleVisits = array('McDonalds', 'Tim Hortons', 'Lakehead Gym', 'Calicos', 'Outpost');
-$possibleDurations = array(30, 60, 90, 120, 150, 180, 210);
+$possibleDurations = array(30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420);
 
 $statement = "INSERT INTO `dummydata` (timestamp, latitude, longitude, speed, steps, bpm, visit, duration) VALUES (?,?,?,?,?,?,?,?)";
 //Loops for two weeks of time using tenmin segments as an iteration
@@ -28,11 +28,17 @@ for($i = 0; $i < 2016; $i++){
     $dummyLat = 'dummyLat';
     $dummyLong = 'dummyLong';
     $dummySpeed = 'dummySpeed';
-    $dummySteps += rand(1, 100);
-    $dummyBPM = rand(60, 140);
+    $dummySteps += rand(0, 75);
+    $BPMChance = rand(1,100);
+    //Only 50% chance of recording BPM
+    if($BPMChance <= 50){
+        $dummyBPM = rand(60, 140);
+    }else{
+        $dummyBPM = '';
+    }
     //Only 25% chance of a visit occurring
     $visitChance = rand(1,100);
-    if($visitChance <= 25){
+    if($visitChance <= 5){
         $dummyVisit = $possibleVisits[array_rand($possibleVisits, 1)];
         $dummyDur = $possibleDurations[array_rand($possibleDurations, 1)];
     }else{
